fe2de96a6728cb9ebe4d3e0c1cd37a6751d2e6f3
Vitor Alexandre Campos Figueiredo <vitor.figueiredo@inatel.br>
2022-03-10 12:24:03 -0300

    novo repo

---
+++ src/main/java/br/inatel/myrestapi/controller/MyMessage.java
@@ -0,0 +1,17 @@
+package br.inatel.myrestapi.controller;
+
+public class MyMessage {
+	
+	private String info;
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+	
+
+}
